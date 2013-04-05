4f56fc121d166556ef9b49ab4a5aa94ee7bc5257
simonthelwall <bobbins@bobbins.com>
2013-04-05 22:11:36 +0100

    first commit

---
+++ exif_extractor.rb
@@ -0,0 +1,13 @@
+!#/usr/bin/env ruby
+require 'exifr'
+
+# iterate through all files in the folder
+# append exif to new file
+# write file. 
+# profit.
+
+# want out
+# focal length
+# shutter speed
+# aperture
+
